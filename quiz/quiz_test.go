2a017359d8b5d9d2692ba0f58e131fd531f5b500
Kieron Browne <kbrowne@pivotal.io>
2017-11-07 22:17:53 +0000

    Make command exit test explicit and remove dummy test

--- quiz/quiz_test.go
+++ quiz/quiz_test.go
@@ -15,21 +15,19 @@ var _ = Describe("Quiz", func() {
 		It("displays usages given -h flag", func() {
 			command := exec.Command(pathToQuiz, "-h")
 			stdout := gbytes.NewBuffer()
-			_, err := gexec.Start(command, stdout, GinkgoWriter)
+			session, err := gexec.Start(command, stdout, GinkgoWriter)
 			Expect(err).NotTo(HaveOccurred())
-			Eventually(stdout).Should(gbytes.Say("Usage"))
+			Eventually(session).Should(gexec.Exit())
+			Expect(stdout).To(gbytes.Say("Usage"))
 		})
 
 		It("doesn't display usage when no -h flag", func() {
 			command := exec.Command(pathToQuiz)
 			stdout := gbytes.NewBuffer()
-			_, err := gexec.Start(command, stdout, GinkgoWriter)
+			session, err := gexec.Start(command, stdout, GinkgoWriter)
 			Expect(err).NotTo(HaveOccurred())
-			Consistently(stdout).ShouldNot(gbytes.Say("Usage"))
+			Eventually(session).Should(gexec.Exit())
+			Expect(stdout).ToNot(gbytes.Say("Usage"))
 		})
-	})
-
-	It("does something", func() {
-		Expect(true).To(BeTrue())
 	})
 })
